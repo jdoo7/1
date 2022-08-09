5f037d39c7f38b0fcedd477eb2babd325da734d0
Darshan <jasoliyadarshan96@gmail.com>
2022-08-09 21:06:45 +0530

    hello new commit

--- pr/chat/models.py
+++ pr/chat/models.py
@@ -7,8 +7,6 @@ class Chat(models.Model):
     group = models.ForeignKey('Group',on_delete=models.CASCADE)
 class Group(models.Model):
     name = models.CharField(max_length = 255)
-    
-    def __str__(self):
-        return self.name1
+
 
     
