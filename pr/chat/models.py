3796f11f234074066daea48313b2e4814cf7cd15
Darshan <jasoliyadarshan96@gmail.com>
2022-08-09 21:01:05 +0530

    chang in staging branch

--- pr/chat/models.py
+++ pr/chat/models.py
@@ -5,6 +5,10 @@ class Chat(models.Model):
     content = models.CharField(max_length = 1000)
     timestamp = models.DateTimeField(auto_now=True)
     group = models.ForeignKey('Group',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.group
+
 class Group(models.Model):
     name = models.CharField(max_length = 255)
     
